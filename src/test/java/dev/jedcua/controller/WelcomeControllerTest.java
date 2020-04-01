79e7b288a3d1dc2a9a06845c9a6a2328f5a80a8e
jedcua <jonedwardcua@gmail.com>
2020-04-01 12:48:24 +0800

    Added mo tests

---
+++ src/test/java/dev/jedcua/controller/WelcomeControllerTest.java
@@ -0,0 +1,36 @@
+package dev.jedcua.controller;
+
+import dev.jedcua.ui.StageManager;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+@ExtendWith(ApplicationExtension.class)
+public class WelcomeControllerTest {
+    private Stage stage;
+
+    @Start
+    private void start(final Stage stage) {
+        this.stage = stage;
+    }
+
+    @AfterEach
+    public void afterAll() {
+        StageManager.destroy();
+    }
+
+    @Test
+    public void openInventory(final FxRobot robot) {
+        StageManager.initialize(stage, 100, 100);
+        Assertions.assertDoesNotThrow(() -> {
+            robot.interact(() -> {
+                new WelcomeController().openInventory(null);
+            });
+        });
+    }
+}
